302766468df4c78499d06d97a2f1dbdafe0703fa
aleksey <ignatulya@mail.ru>
2022-06-08 10:51:46 +0300

    Kod k Zadacha 23

---
+++ Zadacha_23/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Напишите программу, которая принимает на вход число (N) и выдает таблицу кубов чисел от 1 до N.
+3 -> 1, 4, 9 ?????? Это я так понимаю таблица квадратов, а не кубов.
+5 -> 1, 8, 27, 64, 125
+*/
+
+Console.WriteLine("Введите число N:   ");
+string numberN = Console.ReadLine();
+int N = Convert.ToInt32(numberN);
+
+
+int[] GetCubeTable(int N)
+{
+    int[] cubs = new int[N];
+    int number = 1;
+    for (int i = 0; i < N; i++)
+    {
+        cubs[i] = number * number * number;
+        number++;
+    }
+    return cubs;
+}
+
+void TableCubs(int[] array)
+{
+    int number = 1;
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{number} в кубе = ");
+        System.Console.WriteLine(array[i]);
+        number++;
+    }
+}
+
+int[] CubeTable = GetCubeTable(N);
+TableCubs(CubeTable);
